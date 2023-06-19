ee6503e834858dbcd72b8cbb40f391ed708b8253
Samuel Gonçalves <samuel.c.goncalves07@gmail.com>
2023-06-19 21:14:07 +0200

    LIB-10 Tests

--- src/main/java/com/cgdp/library/dto/book/CreateBookRequestDTO.java
+++ src/main/java/com/cgdp/library/dto/book/CreateBookRequestDTO.java
@@ -3,8 +3,10 @@ package com.cgdp.library.dto.book;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
+import lombok.Builder;
 import org.hibernate.validator.constraints.ISBN;
 
+@Builder
 public record CreateBookRequestDTO(@NotBlank String title,
                                    @NotNull Long authorId,
                                    LocalDate publicationYear,
