437db387fceaa42f1891a9a3a10f62e3bd0949e9
samuel.goncalves <samuel.c.goncalves07@gmail.com>
2023-09-03 22:24:39 +0100

    Update ApiExceptionHandler.java

--- catalog/catalog-api/src/main/java/com/cgdpd/library/catalog/api/aop/ApiExceptionHandler.java
+++ catalog/catalog-api/src/main/java/com/cgdpd/library/catalog/api/aop/ApiExceptionHandler.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class ApiExceptionHandler {
 
     @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<Object> requestHandlingNoHandlerFound(final NotFoundException e) {
+    public ResponseEntity<String> resourceNotFoundException(final NotFoundException e) {
         // TODO: 2023-09-03 improve message
         return new ResponseEntity<>("Resource not found", NOT_FOUND);
     }
