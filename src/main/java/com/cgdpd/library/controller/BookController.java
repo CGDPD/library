ed74acc52dd54b8bdfa3b63db66a36143b9cfd56
Moisés Gonçalves <moisesgoncalves1986@gmail.com>
2023-09-11 21:27:14 +0100

    LIB 22 add searchBook method

--- src/main/java/com/cgdpd/library/controller/BookController.java
+++ src/main/java/com/cgdpd/library/controller/BookController.java
@@ -3,6 +3,9 @@ package com.cgdpd.library.controller;
 import com.cgdpd.library.dto.book.CreateBookRequestDTO;
 import com.cgdpd.library.dto.book.CreateBookResponseDTO;
 import com.cgdpd.library.dto.book.DetailedBookDTO;
+import com.cgdpd.library.dto.book.SearchBookCriteria;
+import com.cgdpd.library.dto.pagination.PagedResponse;
+import com.cgdpd.library.dto.pagination.PaginationCriteria;
 import com.cgdpd.library.service.BookService;
 import com.cgdpd.library.type.Isbn13;
 
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,5 +38,13 @@ public class BookController {
     @ResponseStatus(HttpStatus.OK)
     public DetailedBookDTO getBook(@PathVariable Isbn13 isbn13) {
         return bookService.getDetailedBookByIsbn13(isbn13);
+    }
+
+    @GetMapping("/book")
+    @ResponseStatus(HttpStatus.OK)
+    public PagedResponse<DetailedBookDTO> searchBook(
+          @RequestParam(required = false) SearchBookCriteria searchBookCriteria,
+          PaginationCriteria paginationCriteria) {
+        return bookService.findDetailedBooksPage(paginationCriteria, searchBookCriteria);
     }
 }
