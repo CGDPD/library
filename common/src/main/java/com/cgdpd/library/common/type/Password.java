a90559ed832a05376eddbe7ee4e561e92f397fcb
Samuel Gonçalves <samuel.c.goncalves07@gmail.com>
2023-09-19 13:47:46 +0100

    LIB-31 Postman collection

--- common/src/main/java/com/cgdpd/library/common/type/Password.java
+++ common/src/main/java/com/cgdpd/library/common/type/Password.java
@@ -2,10 +2,15 @@ package com.cgdpd.library.common.type;
 
 import static com.cgdpd.library.common.validation.Validator.requiredNotBlank;
 
-public record Password(String value) {
+import com.cgdpd.library.common.type.serializer.TypeSerializer;
 
-    public Password {
-        requiredNotBlank("value", value);
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+@JsonSerialize(using = TypeSerializer.class)
+public class Password extends Type<String> {
+
+    private Password(String password) {
+        super(requiredNotBlank("password", password));
     }
 
     public static Password of(String value) {
