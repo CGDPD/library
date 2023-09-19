44985a6348e5f808ca7f7ef2831470b634cf05e7
Samuel Gonçalves <samuel.c.goncalves07@gmail.com>
2023-09-19 19:36:42 +0100

    LIB-22 Delete old code

--- src/main/java/com/cgdpd/library/model/user/Gender.java
+++
@@ -1,6 +0,0 @@
-package com.cgdpd.library.model.user;
-
-public enum Gender {
-    MALE,
-    FEMALE
-}
--- src/testFunctional/resources/db/migration/V001.6__ALTER_TABLE_USERS_FIX_GENDER.sql
+++
@@ -1,8 +0,0 @@
-alter table users
-    drop constraint if exists users_gender_check;
-
-alter table users
-    add constraint users_gender_check check (gender in ('MALE', 'FEMALE'));
-
-update users
-set gender = upper(gender);
